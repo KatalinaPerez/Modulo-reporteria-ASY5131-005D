4917e7daeb2efba3c7828c6a14259dd9a821420a
kata.perezg@duocuc.cl <kata.perezg@duocuc.cl>
2025-05-05 10:53:18 -0400

    Boceto para llamr apis y sus test editado

--- src/keys.py
+++ src/keys.py
@@ -28,6 +28,7 @@ cargar_secretos_doppler()
 # Variables de entorno disponibles en el código
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
 AWS_SESSION_TOKEN = os.getenv('AWS_SESSION_TOKEN')  # Puedes ignorarlo si no lo usas
 REGION_NAME = os.getenv('AWS_REGION', 'us-east-1')  # Valor por defecto
 BUCKET_NAME = os.getenv('BUCKET_NAME', 'modreporteria')  # Valor por defecto
