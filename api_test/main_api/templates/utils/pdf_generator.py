38fd92a5e04f622c4035d62e350c7e2177565524
kata.perezg@duocuc.cl <kata.perezg@duocuc.cl>
2025-06-20 15:45:18 -0400

    Cont, Stock y Adqui bien llamadas, falta lo otro

--- api_test/main_api/templates/utils/pdf_generator.py
+++ api_test/main_api/templates/utils/pdf_generator.py
@@ -110,6 +110,56 @@ def generar_reporte_stock(stock):
     print("Tamaño del PDF generado:", len(pdf_bytes))
     return pdf_bytes
 
+def generar_reporte_adqui(adquisiciones):
+    print("🟣 Iniciando generación del PDF de adquisiciones")
+
+    # PDF personalizado
+    class PDF(FPDF):
+        def header(self):
+            self.set_font("Arial", "B", 12)
+            self.cell(0, 10, "Reporte de Adquisiciones - OBUMA", 0, 1, "C")
+            self.ln(5)
+
+        def footer(self):
+            self.set_y(-15)
+            self.set_font("Arial", "I", 8)
+            self.cell(0, 10, f"Página {self.page_no()}", 0, 0, "C")
+
+    pdf = PDF()
+    pdf.add_page()
+    pdf.set_font("Arial", "B", 14)
+    pdf.cell(0, 10, "Listado de Adquisiciones", 0, 1, "C")
+    pdf.ln(10)
+
+    # Tabla
+    pdf.set_font("Arial", "B", 10)
+    pdf.cell(10, 10, "ID", 1, 0, "C")
+    pdf.cell(40, 10, "Fecha", 1, 0, "C")
+    pdf.cell(40, 10, "Precio de Compra", 1, 0, "C")
+    pdf.cell(30, 10, "Cantidad", 1, 1, "C")
+    pdf.cell(40, 10, "Producto", 1, 1, "C")
+
+    pdf.set_font("Arial", "", 10)
+    # aqui llaman a los datos tal cual salen en las bd de cada equipo.
+    for adq in adquisiciones:
+        # variable = str/cont (segun el tipo de dato que sea).get("nom igual a la bd", "valor por defecto si no existe")
+        id_adq = str(adq.get("id", ""))
+        fecha_adq = adq.get("fecha", {})
+        precio_compra = str(adq.get("precio_compra", ""))
+        cantidad = str(adq.get("cantidad", ""))
+        producto = str(adq.get("producto", ""))
+
+        pdf.cell(10, 10, id_adq, 1, 0, "L")
+        pdf.cell(40, 10, fecha_adq, 1, 0, "L")
+        pdf.cell(40, 10, precio_compra, 1, 0, "L")
+        pdf.cell(30, 10, cantidad, 1, 1, "L")
+        pdf.cell(40, 10, producto, 1, 1, "L")
+
+    pdf_bytes = pdf.output(dest='S').encode('latin1')
+    print("Tamaño del PDF generado:", len(pdf_bytes))
+    return pdf_bytes
+
+
 def generar_reporte_prov_pedido(proveedores):
     print("🟣 Iniciando generación del PDF de proveedores")
 
@@ -156,53 +206,6 @@ def generar_reporte_prov_pedido(proveedores):
     print("Tamaño del PDF generado:", len(pdf_bytes))
     return pdf_bytes
 
-def generar_reporte_adqui(adquisiciones):
-    print("🟣 Iniciando generación del PDF de adquisiciones")
-
-    # PDF personalizado
-    class PDF(FPDF):
-        def header(self):
-            self.set_font("Arial", "B", 12)
-            self.cell(0, 10, "Reporte de Adquisiciones - OBUMA", 0, 1, "C")
-            self.ln(5)
-
-        def footer(self):
-            self.set_y(-15)
-            self.set_font("Arial", "I", 8)
-            self.cell(0, 10, f"Página {self.page_no()}", 0, 0, "C")
-
-    pdf = PDF()
-    pdf.add_page()
-    pdf.set_font("Arial", "B", 14)
-    pdf.cell(0, 10, "Listado de Adquisiciones", 0, 1, "C")
-    pdf.ln(10)
-
-    # Tabla
-    pdf.set_font("Arial", "B", 10)
-    pdf.cell(60, 10, "ID Asiento", 1, 0, "C")
-    pdf.cell(40, 10, "Fecha", 1, 0, "C")
-    pdf.cell(40, 10, "Descripción", 1, 0, "C")
-    pdf.cell(40, 10, "Referencia", 1, 1, "C")
-
-    pdf.set_font("Arial", "", 10)
-    # aqui llaman a los datos tal cual salen en las bd de cada equipo.
-    for adq in adquisiciones:
-        # variable = str/cont (segun el tipo de dato que sea).get("nom igual a la bd", "valor por defecto si no existe")
-        id_adq = adq.get("id", "")[:30]
-        fecha_adq = adq.get("fecha", {})
-        precio_compra = str(adq.get("precio_compra", ""))
-        cantidad = str(adq.get("cantidad", ""))
-        producto = str(adq.get("producto", ""))
-
-        pdf.cell(60, 10, id_adq, 1, 0, "L")
-        pdf.cell(40, 10, fecha_adq, 1, 0, "L")
-        pdf.cell(40, 10, precio_compra, 1, 0, "L")
-        pdf.cell(40, 10, cantidad, 1, 1, "L")
-        pdf.cell(40, 10, producto, 1, 1, "L")
-
-    pdf_bytes = pdf.output(dest='S').encode('latin1')
-    print("Tamaño del PDF generado:", len(pdf_bytes))
-    return pdf_bytes
 
 #Viene desde api de internet
 def generar_reporte_products(productos):
